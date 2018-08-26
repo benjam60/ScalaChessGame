db7482c7fd501bf5398728040057de5e52d9857d
benjam60 <erichseb@umich.edu>
2018-08-26 10:15:42 -0400

    fixed test case and fully finished board with rank and file

--- src/main/scala/steps/CucumberHelperFunctions.scala
+++ src/main/scala/steps/CucumberHelperFunctions.scala
@@ -1,9 +1,10 @@
 package steps
 
-import ChessGame.ChessBoard
+import ChessGame.{ChessBoard, ChessPieceDisplayNames}
 import cucumber.api.DataTable
+import ChessGame.ChessPieceDisplayNames.Space
 import scala.collection.JavaConverters._
-
+import ChessGame.ChessBoardUtilityFunctions.addSpacing
 
 object CucumberHelperFunctions {
 
@@ -13,12 +14,14 @@ object CucumberHelperFunctions {
     def removeRankNumbersAndChangeType(list: List[String]): List[List[String]] = {
       list match {
         case Nil => Nil
-        case _ => list.slice(1, rowSize) :: removeRankNumbersAndChangeType(list.drop(rowSize))
+        case _ => list.slice(1, rowSize).map(emptyStringToEmptyCell) :: removeRankNumbersAndChangeType(list.drop(rowSize))
       }
     }
+    //.map(q => if (q.size == 2) {q + " "})
     val chessBoardState = removeRankNumbersAndChangeType(boardWithoutFileLetters)
     new ChessBoard(chessBoardState)
   }
+  private def emptyStringToEmptyCell(str : String) = if (str == "") addSpacing(Space) else str
 
   def convertMoves(dataTable: DataTable): List[Map[String, Int]] = {
     dataTable.asMaps(classOf[String], classOf[Int]).asScala.map(_.asScala.toMap).toList
