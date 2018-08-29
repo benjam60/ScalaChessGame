661d05f07616dd0e8f63dc5a299f486f630f698f
benjam60 <erichseb@umich.edu>
2018-08-29 13:08:50 -0400

    fixed the move function to work with rank and file

--- src/main/scala/ChessBoard.scala
+++ src/main/scala/ChessBoard.scala
@@ -1,6 +1,6 @@
 package ChessGame
 import ChessPieceDisplayNames._
-import ChessBoardUtilityFunctions.{writeToBoardCell, addSpacing, formatRow}
+import ChessBoardUtilityFunctions.{writeToBoardCell, addSpacing, formatRow, convertFileToIndex}
 
   class ChessBoard(boardState: List[List[String]]) {
     val board: List[List[String]] = boardState
@@ -10,30 +10,29 @@ import ChessBoardUtilityFunctions.{writeToBoardCell, addSpacing, formatRow}
       val fileLettersRow = List(topLeftCorner, "A", "B", "C", "D", "E", "F", "G", "H").map(addSpacing)
       val getRowsWithRank = boardState.zipWithIndex
       formatRow(fileLettersRow) + getRowsWithRank.map {
-        case (row: List[String], rank: Int) => "|" + addSpacing(rank.toString) + formatRow(row)                        //"| " + rank + " | " + row.mkString("|") + " |"
+        case (row: List[String], rank: Int) => "|" + addSpacing(rank.toString) + formatRow(row)
       }.mkString("")
     }
 
     def movePiece(sourceRank : Int, sourceFile : Char, destRank : Int, destFile : Char) : ChessBoard = {
-      val pieceValue = board(sourceFile)(sourceRank)
-      val erasedLocationName = s"$sourceFile,$sourceRank"
-      val boardWithErasedSource = writeToBoardCell(board, sourceFile, sourceRank, erasedLocationName)
-      new ChessBoard(writeToBoardCell(boardWithErasedSource, destFile, destRank, pieceValue))
+      val pieceToMove = board(sourceRank)(convertFileToIndex(sourceFile))
+      val boardWithSpaceAtSource : List[List[String]] =
+        writeToBoardCell(board, sourceRank, convertFileToIndex(sourceFile), Space)
+      new ChessBoard(writeToBoardCell(boardWithSpaceAtSource, destRank, convertFileToIndex(destFile), pieceToMove))
     }
   }
 
 object ChessBoardUtilityFunctions {
-  def writeToBoardCell(board : List[List[String]], file : Char, rank : Int, piece : String) : List[List[String]] = {
-    val fileInt = convertFileToIndex(file)
-    val numberOfRowsBeforeEditedRow = fileInt
-    val indexOfRowAfterEditedRow = fileInt + 1
+  def writeToBoardCell(board : List[List[String]], rank : Int, file : Int, piece : String) : List[List[String]] = {
+    val numberOfRowsBeforeEditedRow = rank
+    val indexOfRowAfterEditedRow = rank + 1
     board.take(numberOfRowsBeforeEditedRow) ++
-      List(board(file).patch[String, List[String]](rank, Seq(piece), replaced = 1)) ++
+      List(board(rank).patch[String, List[String]](file, Seq(piece), replaced = 1)) ++
       board.drop(indexOfRowAfterEditedRow)
   }
   def addSpacing(boardCell : String) = if (boardCell.size != 3) { " " + boardCell + " "} else boardCell
   def formatRow(row : List[String]) =  "|" + row.map(addSpacing).mkString("|") + "|\n"
-  private def convertFileToIndex(file : Char) : Int = file.toInt - 65
+  def convertFileToIndex(file : Char) : Int = file.toInt - 65
 }
 
   object InitialChessBoardState {
