9d2f6d82ff58100083c7c12e53bcef289febdd33
benjam60 <erichseb@umich.edu>
2018-09-01 16:53:28 -0400

    Fixed bug, using wrong val in chessboard

--- src/main/scala/ChessBoard.scala
+++ src/main/scala/ChessBoard.scala
@@ -9,7 +9,8 @@ class ChessBoard(boardState: List[List[String]]) {
   def movePiece(sourceRank: Int, sourceFile: Char, destRank: Int, destFile: Char): ChessBoard = {
     val pieceToMove = getPiece(sourceRank, sourceFile)
     val boardWithDeletedPiece: ChessBoard = deletePiece(this, sourceRank, sourceFile)
-    setPiece(boardWithDeletedPiece, pieceToMove, destRank, destFile)
+    val x = setPiece(boardWithDeletedPiece, pieceToMove, destRank, destFile)
+    x
   }
 
   override def toString: String = {
@@ -21,30 +22,29 @@ class ChessBoard(boardState: List[List[String]]) {
     }.mkString("")
   }
 
-
   private def getPiece(rankIn : Int, fileIn : Char) : String = {
-    val(rank, file) = boardStateIndexes(rankIn, fileIn)
-    state(rank)(file)
+    val(row, col) = boardStateIndexes(rankIn, fileIn)
+    state(row)(col)
   }
 
   private def deletePiece(board: ChessBoard, rank : Int, file : Char) : ChessBoard =
     setPiece(board, Space, rank, file)
 
   private def setPiece(board: ChessBoard, piece : String, rankIn : Int, fileIn : Char) : ChessBoard = {
-    val(rank, file) = boardStateIndexes(rankIn, fileIn)
-    new ChessBoard(overwriteCell(board.state, piece, rank, file))
+    val(row, col) = boardStateIndexes(rankIn, fileIn)
+    new ChessBoard(overwriteCell(board.state, piece, row, col))
   }
-  
+
   private def overwriteCell(boardState: List[List[String]], piece : String, row : Int, col : Int) : List[List[String]] = {
     val numberOfRowsBeforeEditedRow = row
     val indexOfRowAfterEditedRow = row + 1
-    state.take(numberOfRowsBeforeEditedRow) ++ List(state(row).patch[String, List[String]](col, Seq(piece), replaced = 1)) ++
-      state.drop(indexOfRowAfterEditedRow)
+      boardState.take(numberOfRowsBeforeEditedRow) ++
+      List(boardState(row).patch[String, List[String]](col, Seq(piece), replaced = 1)) ++
+      boardState.drop(indexOfRowAfterEditedRow)
   }
 }
 
 object ChessBoardUtilityFunctions {
-  private val StandardPieceSize = 3
   def addSpacing(boardCell: String) = if (boardCell.size != StandardPieceSize) {
     " " + boardCell + " "
   } else boardCell
@@ -54,6 +54,7 @@ object ChessBoardUtilityFunctions {
   def formatRow(row: List[String]) = "|" + row.map(addSpacing).mkString("|") + "|\n"
 
   def convertFileToIndex(file: Char): Int = file.toInt - 65
+  private val StandardPieceSize = 3
 }
 
 object InitialChessBoardState {
