75f7e573cd5c8cc7b5bf2197a8c2fabd4a277230
benjam60 <erichseb@umich.edu>
2019-06-25 11:13:56 -0400

    Bettering the style

--- src/main/scala/PieceMovement.scala
+++ src/main/scala/PieceMovement.scala
@@ -4,16 +4,19 @@ import ChessGame.AllPieces._
 
 object PieceMovement {
 
-  def movePiece(board: Board, piece: ChessPiece, source : BoardPosition, destination : BoardPosition): Board = {
-			val removedSourcePiece = updateBoard(board, Option.empty[ChessPiece], source)
-			updateBoard(removedSourcePiece, changePawnType(piece, source, destination), destination)
-		}
+  def movePiece(board: Board, piece: ChessPiece, source : BoardPosition, destination : BoardPosition): Board =
+			updateBoard(removeSourcePiece(board, source), changePawnType(piece, source, destination), destination)
 
-	 private def updateBoard(board : Board, piece: Option[ChessPiece], position : BoardPosition): Board = {
-		 val updatedRow = board.state(position.rankBoardIndex).updated(position.fileBoardIndex, piece)
-		 val updatedState = board.state.updated(position.rankBoardIndex, updatedRow)
-		 board.copy(updatedState)
-	 }
+	private def updateBoard(board : Board, piece: Option[ChessPiece], position : BoardPosition): Board = {
+		val updatedState = board.state.updated(position.rankBoardIndex, placePiece(board, piece, position))
+		board.copy(updatedState)
+	}
+
+	private def placePiece(board : Board, piece: Option[ChessPiece], position : BoardPosition) =
+		board.state(position.rankBoardIndex).updated(position.fileBoardIndex, piece)
+
+	private def removeSourcePiece(board: Board, source : BoardPosition) : Board =
+		updateBoard(board, Option.empty[ChessPiece], source)
 
   private def changePawnType(pieceToMove : ChessPiece, source : BoardPosition, destination : BoardPosition) : Option[ChessPiece] =
     if (Math.abs(destination.rankBoardIndex - source.rankBoardIndex) == 2) { //can abstract as Pawn!!
