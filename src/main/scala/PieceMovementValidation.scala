1f10ec0418c980fa9b9e2ea1c53a0df867c4fa1a
benjam60 <erichseb@umich.edu>
2019-08-18 10:52:16 -0400

    Removing used functions

--- src/main/scala/PieceMovementValidation.scala
+++ src/main/scala/PieceMovementValidation.scala
@@ -5,14 +5,6 @@ import ChessGame.AllPieces.ChessPiece
 //refactor this so it is easy to add and remove rules
 object PieceMovementValidation {
 
-	//Boolean, Option[ErrorType]
-	def isValidMove(board: Board, source: BoardPosition, destination: BoardPosition,
-	                turn: Color, pieceToMove: Option[ChessPiece]) : Boolean =
-		pieceToMove.exists(piece => piece.isValidMove(board, source, destination) &&
-			playerIsMovingOwnPiece(turn, piece) && !arePiecesInBetween(board, source, destination))
-
-	private def playerIsMovingOwnPiece(turn : Color, piece : ChessPiece) : Boolean = piece.getColor == turn
-
 	def arePiecesInBetween(board: Board, source: BoardPosition, destination: BoardPosition) : Boolean = {
 		if (isMovingVertically(source, destination) && verticalDistance(source, destination) >= minimumLengthThatCouldHavePieces) {
 			val (minRank, maxRank) = if (source.rankBoardIndex > destination.rankBoardIndex)
